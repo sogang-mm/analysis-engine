5ac82afbd5efed389fc2429e4941ccc3450121bc
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 08:31:31 +0900

    Modify secret key from static to dynamic

--- AnalysisModule/settings.py
+++ AnalysisModule/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+
+from AnalysisModule.utils import secret_key
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7yy=zh6zad^z2tn&^eyuy@6359g9l4g$$35=sqg@jkw@n=e-@o'
+SECRET_KEY = str(secret_key.SECRET_KEY())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
