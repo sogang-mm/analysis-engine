bdd07589c2e62f3ee5f67242c215e669d9ddd48c
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 06:28:59 +0900

    Modify settings.py - Add static dir

--- AnalysisModule/settings.py
+++ AnalysisModule/settings.py
@@ -128,6 +128,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
 
