422de8b5361438ff97d3801dfcaaa0a0ad0e8cb4
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 06:30:22 +0900

    Add VideoModel and AudioModel class

--- WebAnalyzer/models.py
+++ WebAnalyzer/models.py
@@ -4,31 +4,92 @@ import os
 
 from django.db import models
 
-# Create your models here.
 from rest_framework import exceptions
-from AnalysisModule.config import DEBUG
-from WebAnalyzer.tasks import analyzer_by_path
-from WebAnalyzer.utils import filename
-from django_mysql.models import JSONField
 
+from AnalysisModule import settings
+from AnalysisModule.config import DEBUG
+from WebAnalyzer.tasks import analyzer_by_image, analyzer_by_video
+from WebAnalyzer.utils import filename
+from WebAnalyzer.utils.media import *
+from django_mysql.models import JSONField
 import ast
 
 
 class ImageModel(models.Model):
-    image = models.FileField(upload_to=filename.default)
+    image = models.ImageField(upload_to=filename.default)
     token = models.AutoField(primary_key=True)
     uploaded_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     result = JSONField(null=True)
 
-
     def save(self, *args, **kwargs):
         super(ImageModel, self).save(*args, **kwargs)
 
         if DEBUG:
-            task_get = ast.literal_eval(str(analyzer_by_path(self.image.path)))
+            task_get = ast.literal_eval(str(analyzer_by_image(self.image.path, 'image')))
         else:
-            task_get = ast.literal_eval(str(analyzer_by_path.delay(self.image.path).get()))
+            task_get = ast.literal_eval(str(analyzer_by_image.delay(self.image.path, 'image').get()))
 
         self.result = task_get
         super(ImageModel, self).save()
+
+
+class VideoModel(models.Model):
+    video = models.FileField(upload_to=filename.default, null=True)
+    video_url = models.TextField(max_length=255, null=True)
+    video_info = JSONField(null=True)
+    video_text = models.TextField(null=True)
+    extract_fps = models.IntegerField(default=1)
+    analysis_type = models.TextField(max_length=255, null=False)
+    token = models.AutoField(primary_key=True)
+    uploaded_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    result = JSONField(null=True)
+
+    def save(self, *args, **kwargs):
+        super(VideoModel, self).save(*args, **kwargs)
+
+        if self.analysis_type == 'audio':
+            audio = self.audio.create()
+            data = audio.audio.path
+        elif self.analysis_type == 'text':
+            data = self.video_text
+        else :
+            if self.video_url is not None:
+                video_path = self.video_url
+            else :
+                video_path = self.video.path
+            data, urls = extract_frames(video_path, self.extract_fps)
+            for frame_url in urls:
+                self.frame.create(frame=frame_url)
+
+            self.video_info = get_video_metadata(video_path)
+
+        if DEBUG:
+            task_get = ast.literal_eval(str(analyzer_by_video(data, self.video_info, self.analysis_type)))
+        else:
+            task_get = ast.literal_eval(str(analyzer_by_video.delay(data, self.video_info, self.analysis_type).get()))
+
+        self.result = task_get
+        super(VideoModel, self).save()
+
+
+class AudioModel(models.Model):
+    video = models.ForeignKey(VideoModel, related_name='audio', on_delete=models.CASCADE, null=True)
+    audio = models.FileField(upload_to=filename.default)
+    token = models.AutoField(primary_key=True)
+    uploaded_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        super(AudioModel, self).save(*args, **kwargs)
+        if self.video != None:
+            audio_path = extract_audio(self.video.video_url)
+            self.audio = audio_path
+
+        super(AudioModel, self).save()
+
+
+class FrameModel(models.Model):
+    video = models.ForeignKey(VideoModel, related_name='frame', on_delete=models.CASCADE, null=True)
+    frame = models.ImageField(max_length=255)
