027a2615c6d053c9b94de6f03f5d91289ec1dc9c
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 06:31:05 +0900

    Add VideoSerializer AudioSerializer

--- WebAnalyzer/serializers.py
+++ WebAnalyzer/serializers.py
@@ -9,3 +9,19 @@ class ImageSerializer(serializers.HyperlinkedModelSerializer):
         model = ImageModel
         fields = ('image', 'token', 'uploaded_date', 'updated_date', 'result')
         read_only_fields = ('token', 'uploaded_date', 'updated_date', 'result')
+
+
+class VideoSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = VideoModel
+        fields = ('video', 'video_url', 'video_info', 'video_text', 'analysis_type', 'token', 'uploaded_date', 'updated_date', 'result')
+        read_only_fields = ('audio', 'token', 'uploaded_date', 'updated_date', 'result')
+
+
+class AudioSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = AudioModel
+        fields = ('video', 'audio', 'token', 'uploaded_date', 'updated_date')
+        read_only_fields = ('video', 'token', 'uploaded_date', 'updated_date')
