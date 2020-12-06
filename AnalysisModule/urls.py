c5588d4666531afc9951e67455dc81085fbba326
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 06:34:12 +0900

    Add urls

--- AnalysisModule/urls.py
+++ AnalysisModule/urls.py
@@ -1,22 +1,5 @@
-"""AnalysisModule URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
-from django.conf.urls import url
 from django.contrib import admin
 
 from django.conf import settings
@@ -29,9 +12,11 @@ import WebAnalyzer.views
 
 router = DefaultRouter()
 
-router.register(r'', WebAnalyzer.views.ImageViewSet)
+router.register(r'image', WebAnalyzer.views.ImageViewSet)
+router.register(r'video', WebAnalyzer.views.VideoViewSet)
+router.register(r'audio', WebAnalyzer.views.AudioViewSet)
 
-urlpatterns = [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
