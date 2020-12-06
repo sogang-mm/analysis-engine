e70923d10963a15efdbeeb5a9ab7510688807f8a
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 06:32:52 +0900

    Add VideoViewSet and AudioViewSet

--- WebAnalyzer/views.py
+++ WebAnalyzer/views.py
@@ -3,15 +3,47 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
 from WebAnalyzer.models import *
 from WebAnalyzer.serializers import *
 from rest_framework import viewsets, generics
 
 
 class ImageViewSet(viewsets.ModelViewSet):
+
     queryset = ImageModel.objects.all()
     serializer_class = ImageSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        queryset = queryset.order_by('-token')
+
+        token = self.request.query_params.get('token', None)
+        if token is not None:
+            queryset = queryset.filter(token=token)
+
+        return queryset
+
+
+class VideoViewSet(viewsets.ModelViewSet):
+
+    queryset = VideoModel.objects.all()
+    serializer_class = VideoSerializer
+
+    def get_queryset(self):
+        queryset = self.queryset
+        queryset = queryset.order_by('-token')
+
+        token = self.request.query_params.get('token', None)
+        if token is not None:
+            queryset = queryset.filter(token=token)
+
+        return queryset
+
+
+class AudioViewSet(viewsets.ModelViewSet):
+
+    queryset = AudioModel.objects.all()
+    serializer_class = AudioSerializer
 
     def get_queryset(self):
         queryset = self.queryset
