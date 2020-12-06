147a962ae97ed6970fd8a92c0f1af3f01d12f23c
DESKTOP-MMLAB\JinhaSong <the_jn@naver.com>
2020-12-07 06:33:27 +0900

    Add models in admin page

--- WebAnalyzer/admin.py
+++ WebAnalyzer/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from WebAnalyzer import models
 
-# Register your models here.
+admin.site.register(models.ImageModel)
+admin.site.register(models.VideoModel)
+admin.site.register(models.FrameModel)
+admin.site.register(models.AudioModel)
